a28b8ccc63d9736c764f515f51422d681f92fe9d
z15r7 <z15r7x@gmail.com>
2022-08-09 03:04:44 +0800

    Update

--- frontend/src/components/Main/Selector.jsx
+++ frontend/src/components/Main/Selector.jsx
@@ -3,7 +3,7 @@ import React, {
 } from 'react';
 
 import {
-  FormControl, InputLabel, Select, MenuItem,
+  Box, FormControl, InputLabel, Select, MenuItem, Typography,
 } from '@mui/material';
 
 const Selector = () => {
@@ -14,20 +14,27 @@ const Selector = () => {
   };
 
   return (
-    <FormControl sx={{ width: '100%' }}>
-      <InputLabel id={'select-label'}>{'Age'}</InputLabel>
-      <Select
-        labelId={'select-label'}
-        id={'select'}
-        value={age}
-        label={'Age'}
-        onChange={handleChange}
-      >
-        <MenuItem value={10}>{'Ten'}</MenuItem>
-        <MenuItem value={20}>{'Twenty'}</MenuItem>
-        <MenuItem value={30}>{'Thirty'}</MenuItem>
-      </Select>
-    </FormControl>
+    <>
+      <Box>
+        <Typography>{'我要找'}</Typography>
+      </Box>
+      <Box>
+        <FormControl sx={{ width: '100%' }}>
+          <InputLabel id={'select-label'}>{'Age'}</InputLabel>
+          <Select
+            labelId={'select-label'}
+            id={'select'}
+            value={age}
+            label={'Age'}
+            onChange={handleChange}
+          >
+            <MenuItem value={10}>{'Ten'}</MenuItem>
+            <MenuItem value={20}>{'Twenty'}</MenuItem>
+            <MenuItem value={30}>{'Thirty'}</MenuItem>
+          </Select>
+        </FormControl>
+      </Box>
+    </>
   );
 };
 
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 
+import { Grid } from '@mui/material';
 import Main from '../components/Main/Main';
 
 const Home = () => (
-  <Main />
+  <Grid container spacing={0}>
+    <Grid item md={2} />
+    <Grid item xs={12} md={8}>
+      <Main />
+    </Grid>
+    <Grid item md={2} />
+  </Grid>
 );
 
 export default Home;
